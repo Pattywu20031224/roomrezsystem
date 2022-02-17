0e7f81c2bcdd661e86945130f1e64daf34d8e2e4
Pattywu20031224 <10835096@dcsh.tp.edu.tw>
2022-02-17 23:11:32 +0800

    first commit

---
+++ roomrezsystem/roomrezsystem/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for roomrezsystem project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'roomrezsystem.settings')
+
+application = get_wsgi_application()
