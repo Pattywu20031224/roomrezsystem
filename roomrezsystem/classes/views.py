a2ff2df3613cb9bbaa548066680809533ec17c5f
Pattywu20031224 <10835096@dcsh.tp.edu.tw>
2022-03-10 08:51:03 +0800

    0310_classes_detail

--- roomrezsystem/classes/views.py
+++ roomrezsystem/classes/views.py
@@ -4,6 +4,7 @@ from django.urls import reverse
 from django.views.generic import *
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import *
+from student.models import *
 
 # Create your views here.
 
@@ -17,6 +18,11 @@ class ClassesView(LoginRequiredMixin, DetailView):
 
     ordering = ['name']    
     paginate_by = 20
+    def get_context_data(self,**kwargs):
+        context=super().get_context_data(**kwargs)
+        student_list = Student.objects.filter(classes=self.kwargs['pk'])
+        context['student_list']=student_list
+        return context
 
 class ClassesAdd(LoginRequiredMixin, CreateView):  
     model = Classes
