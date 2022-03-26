efae4c3b154ad1a46605e1e104a6299f537058c3
Pattywu20031224 <10835096@dcsh.tp.edu.tw>
2022-03-26 08:46:46 +0800

    AdminImportTeacher

--- roomrezsystem/roomrezsystem/settings.py
+++ roomrezsystem/roomrezsystem/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'teacher',
     'log',
     'classes',
-    'student'
+    'student',
+    'import_export'
 ]
 
 MIDDLEWARE = [
--- roomrezsystem/teacher/admin.py
+++ roomrezsystem/teacher/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+from .models import Teacher
 
-# Register your models here.
+@admin.register(Teacher)
+class TeacherAdmin(ImportExportModelAdmin):
+    list_display = ('id', 'realname', 'tel','role','cardid')
