6def6e2af400b80d5d7ba07cd41e299449b60b87
seastar <garrylin930728@gmail.com>
2022-03-26 10:00:03 +0800

    newnew

--- roomrezsystem/teacher/admin.py
+++ roomrezsystem/teacher/admin.py
@@ -1,7 +1,1 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from .models import Teacher
-
-@admin.register(Teacher)
-class TeacherAdmin(ImportExportModelAdmin):
-    list_display = ('id', 'realname', 'tel','role','cardid')
